77d3f275ab9256ad194d36c839ebb02797590e8f
janecpark <janecpark@users.noreply.github.com>
2020-06-29 13:44:12 -0700

    Updates

---
+++ precache-manifest.5213559cf7fdd5d7dfe10897462cbbbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b6978ee8d91bb40994897b561c091c",
+    "url": "/lightsout/index.html"
+  },
+  {
+    "revision": "78e67f8b7f8e7a855f85",
+    "url": "/lightsout/static/css/main.3973fc0e.chunk.css"
+  },
+  {
+    "revision": "3970fe950ab44cb515c9",
+    "url": "/lightsout/static/js/2.3cc2883a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lightsout/static/js/2.3cc2883a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e67f8b7f8e7a855f85",
+    "url": "/lightsout/static/js/main.1d3128c0.chunk.js"
+  },
+  {
+    "revision": "02f5e4a2b8844ebeb536",
+    "url": "/lightsout/static/js/runtime-main.1d27a793.js"
+  }
+]);
